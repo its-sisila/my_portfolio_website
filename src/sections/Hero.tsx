fc6bd4be0f430b3e5f4a5b5946af0f8ca2a1af2e
Sisila Dhevasirinarayana <sisila8400@gmail.com>
2025-05-31 22:57:32 +0530

    feat: added hero rings and made it responsive

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,17 @@
 import memojiImage from '@/assets/images/memoji_sisila_no_bg.png';
 import Image from 'next/image';
 import ArrowDown from '@/assets/icons/arrow-down.svg';
+import grainImage from '@/assets/images/grain.jpg';
+import StarIcon from '@/assets/icons/star.svg';
 
 export const HeroSection = () => {
   return (
-    <div className='py-32'>
+    <div className='py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip'>
+      <div className='absolute inset-0 -z-30 opacity-5' style={{ backgroundImage: `url(${grainImage.src})`,}}></div>
+      <div className='size-[620px] hero-ring'></div>
+      <div className='size-[820px] hero-ring'></div>
+      <div className='size-[1020px] hero-ring'></div>
+      <div className='size-[1220px] hero-ring'></div>
       <div className="container">
         <div className='flex flex-col items-center'>
           <Image src={memojiImage} 
@@ -16,17 +23,21 @@ export const HeroSection = () => {
             <div className='text-sm font-medium'>Available for new Projects</div>
           </div>
         </div>
-        <h1>Building Exceptional User Experiences</h1>
-        <p>
+        <div className='max-w-lg mx-auto'>
+          <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide'>
+            Building Exceptional User Experiences
+          </h1>
+          <p className='mt-4 text-center text-white/60'>
           I am a passionate front-end developer with a focus on creating intuitive and engaging user interfaces.
           Let`s collaborate to bring your ideas to life!
-        </p>
-        <div>
-          <button>
-            <span>Explore My Work</span>
-            <ArrowDown />
+          </p>
+        </div>
+        <div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4'>
+          <button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl '>
+            <span className='font-semibold'>Explore My Work</span>
+            <ArrowDown className='size-4'/>
           </button>
-          <button>
+          <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl'>
             <span>👋</span>
             <span>Let`s Connect</span>
           </button>
