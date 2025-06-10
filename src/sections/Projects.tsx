20f97aae9f928418ca0e045cd6f542365ef59057
Sisila Dhevasirinarayana <sisila8400@gmail.com>
2025-06-10 19:51:01 +0530

    feat: animation projects section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -64,10 +64,13 @@ export const ProjectsSection = () => {
           See how I transformed concepts into engaging digital experiences.
         </p>
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, projectIndex) => (
             <Card
               key={project.title}
-              className=" px-8 pt-8 pd-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none"
+              className=" px-8 pt-8 pd-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none sticky"
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px)`,
+              }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
